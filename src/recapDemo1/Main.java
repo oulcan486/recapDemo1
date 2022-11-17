6801c021f07a2286e0893989921f94ae95b9b4ac
oulcan486 <61511437+oulcan486@users.noreply.github.com>
2022-11-17 22:50:25 +0300

    firstcommit

---
+++ src/recapDemo1/Main.java
@@ -0,0 +1,15 @@
+package recapDemo1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Operations operasyon=new Operations();
+		operasyon.Topla(4, 5);
+		operasyon.Carp(4, 5);
+		operasyon.Bol(20, 5);
+
+	}
+
+}
---
+++ src/recapDemo1/Operations.java
@@ -0,0 +1,17 @@
+package recapDemo1;
+
+public class Operations {
+
+	public void Topla(int a,int b) {
+		System.out.println(a+b);
+	}
+	
+	public void Carp(int a,int b) {
+		System.out.println(a*b);
+	}
+
+	
+	public void Bol(int a,int b) {
+		System.out.println(a/b);
+	}
+}
